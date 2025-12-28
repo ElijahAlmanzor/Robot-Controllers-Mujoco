37ae3afe86c9e4fcf1fd2856bc3d45c9e8e64d5e
ElijahAlmanzor <elijahdalma@gmail.com>
2025-12-28 17:04:11 +0000

    Added some more interface functions for the control

--- controllers/PID/kinematic_pid.cpp
+++ controllers/PID/kinematic_pid.cpp
@@ -1,35 +1,72 @@
 #include "kinematic_pid.hpp"
 #include <iostream>
 
-// Set target (cartesian target)
-// Set PID Gains (low level and high level)
-// set translation and orientation control
-// Forward PID simulation (high level/ cartesian) - outputs qdes
-// Forward PID low-level (desired angles to joint torques with its own PID) - outputs torque + grav comp
+
+// 1. Set PID Gains (low level and high level) - DONE!
+// 2. Set target (HTM) - DONE!
+// 3. set joint_target - useful for initiating the joint position at the start - DONE
+// 4. convert set targets to position, orientation targets - DONE!
+// 5. Get translation, orientation, and joint error
+// 5.b PID object translation, orientation and joint error
+// 6. Forward PID simulation (high level - cartesian with orientation) - outputs qdes
+// 7. Forward PID low-level (desired angles to joint torques with its own PID) - outputs torque + grav comp
 
 
 void PID_Kinematic::set_position_gains(float kp, float ki, float kd)
 {
     // Function for adjusting the controller position gains
-    PID_Kinematic::hl_pos_Pgain = kp;
-    PID_Kinematic::hl_pos_Igain = ki;
-    PID_Kinematic::hl_pos_Dgain = kd;
+    hl_pos_Pgain = kp;
+    hl_pos_Igain = ki;
+    hl_pos_Dgain = kd;
 }
 
 
 void PID_Kinematic::set_orientation_gains(float kp, float ki, float kd)
 {
     // Function for adjusting the controller position gains
-    PID_Kinematic::hl_ori_Pgain = kp;
-    PID_Kinematic::hl_ori_Igain = ki;
-    PID_Kinematic::hl_ori_Dgain = kd;
+    hl_ori_Pgain = kp;
+    hl_ori_Igain = ki;
+    hl_ori_Dgain = kd;
 }
 
 void PID_Kinematic::set_joint_gains(float kp, float ki, float kd)
 {
     // Function for adjusting the controller position gains
-    PID_Kinematic::ll_Pgain = kp;
-    PID_Kinematic::ll_Igain = ki;
-    PID_Kinematic::ll_Dgain = kd;
+    ll_Pgain = kp;
+    ll_Igain = ki;
+    ll_Dgain = kd;
 }
 
+void PID_Kinematic::set_target_pose(const Eigen::Isometry3d& pose)
+{
+    this->target_pose = pose;
+
+    Eigen::Vector3d tp = pose.translation();
+    Eigen::Quaterniond tq(pose.rotation());
+
+    set_target_position(tp);
+    set_target_orientation(tq);
+}
+
+void PID_Kinematic::set_target_position(const Eigen::Vector3d& position)
+{
+    this->target_position = position;
+
+    // Overwrite the position in pose just in case
+    this->target_pose.translation() = position;
+}
+
+void PID_Kinematic::set_target_orientation(const Eigen::Quaterniond& orientation)
+{
+    this->target_orientation = orientation;
+
+    // Overwrite the orientation in pose just in case
+    this->target_pose.linear() = orientation.toRotationMatrix();
+
+}
+
+void PID_Kinematic::set_target_joint(const Eigen::VectorXd& joint)
+{
+
+    this->target_joint = joint;
+}
--- controllers/PID/kinematic_pid.hpp
+++ controllers/PID/kinematic_pid.hpp
@@ -9,9 +9,18 @@
 class PID_Kinematic
 {
     public:
+
+        // Set the PID gains - later to do - call the PID objects from custom PID class
         void set_position_gains(float kp, float ki, float kd);
         void set_orientation_gains(float kp, float ki, float kd);
         void set_joint_gains(float kp, float ki, float kd);
+
+        // Set the targets - pose, translation, orientation and desired joint angles
+        void set_target_pose(const Eigen::Isometry3d& pose);
+        void set_target_position(const Eigen::Vector3d& position);
+        void set_target_orientation(const Eigen::Quaterniond& orientation);
+        void set_target_joint(const Eigen::VectorXd& joint);
+
 
 
     private:
@@ -29,6 +38,18 @@ class PID_Kinematic
         float ll_Pgain = 0.0f;
         float ll_Igain = 0.0f;
         float ll_Dgain = 0.0f;
+
+        // Declare the variables for setting the targets - pose, translation, and orientation and even joint desired angles
+        Eigen::Isometry3d target_pose = Eigen::Isometry3d::Identity();
+        Eigen::Vector3d target_position = Eigen::Vector3d::Zero();
+        Eigen::Quaterniond target_orientation = Eigen::Quaterniond::Identity();
+        Eigen::VectorXd target_joint;
+
+
+        // Internal position, rotation and joint errors!
+
+
+
         
 
 };
